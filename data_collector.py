97945528ccceb24322128b835d0aef987f676cc5
Yoonsik Jung <developer.kelvin@gmail.com>
2018-11-05 18:36:57 +0900

    v 0.0.2 테스트 모드 추가 기본 값 추가 DB에 추가하는 SQL 추가 왼손 오른손 대소문자 구분 안하도록 변경

--- data_collector.py
+++ data_collector.py
@@ -4,15 +4,17 @@ import sys
 import pymysql
 from env_setting import host, user, password, db, charset
 
+VERSION = '0.0.2'
+TEST_MODE = True
+
 
 class DataCollector:
 
-    def __init__(self, name_eng, name_kor='', age=''):
-        self.dir_name = os.getcwd().replace('\\', '/') + '/' + name_eng
-        self.name_kor = name_kor
-        self.age = age
-
+    def __init__(self, name_eng, name_kor='', age=0):
         self.get_connection()
+        self.dir_name = os.getcwd().replace('\\', '/') + '/' + name_eng
+
+        self.user_idx = self.insert_userinfo(name_eng=name_eng, name_kor=name_kor, age=age)
 
     def get_connection(self):
         is_conn_success = False
@@ -22,7 +24,7 @@ class DataCollector:
                                             user=user,
                                             password=password,
                                             db=db,
-                                            charset='utf8',
+                                            charset=charset,
                                             cursorclass=pymysql.cursors.DictCursor)
             except Exception as e:
                 print("db connection exception occures")
@@ -38,7 +40,7 @@ class DataCollector:
     def __del__(self):
         self.db_disconnect()
 
-    def read_excel(self, file_path, num_of_ex, hand_side, box_size):
+    def read_excel(self, file_path, ex_sequence, hand_side, box_size):
         try:
             experiment_excel = load_workbook(self.dir_name + '/' + file_path)
             data_sheet = experiment_excel['Sheet1']
@@ -66,13 +68,32 @@ class DataCollector:
                     elif idx == 6:
                         detail['distance_2'] = cell.value
 
-                data_list.append(detail)
+                data_list.append((detail['idx'], detail['x-rect'],detail['y-rect'],detail['x-thumb'],detail['y-thumb'],detail['distance_1'],detail['distance_2']))
 
             print(data_list)
+
         except PermissionError as pe:
             print(pe, file_path)
         except Exception as e:
             print(e, file_path)
+
+        if TEST_MODE:
+            sql = 'INSERT INTO hfe_data_test(ex_sequence, hand_side, box_size, user_idx, box_num, x_rect, y_rect, x_thumb, y_thumb, distance_1, distance_2 ) VALUES (' + ex_sequence + ', ' + hand_side + ', ' + box_size + ', ' + str(self.user_idx) + ',%s ,%s, %s, %s, %s, %s, %s)'
+        else:
+            sql = 'INSERT INTO hfe_data(ex_sequence, hand_side, box_size, box_num, x_rect, y_rect, x_thumb, y_thumb, distance_1, distance_2 ) VALUES (' + ex_sequence + ', '+ hand_side+', '+ box_size+',%s ,%s, %s, %s, %s, %s, %s)'
+
+        try:
+            print(sql)
+            conn = self.conn
+            cursor = conn.cursor()
+            cursor.executemany(sql, data_list)
+            conn.commit()
+        except pymysql.err.MySQLError as mse:
+            print(mse)
+        except Exception as e:
+            print(e)
+        finally:
+            cursor.close()
 
     def get_all_file_list(self):
         file_list = os.listdir(self.dir_name)
@@ -80,8 +101,36 @@ class DataCollector:
 
         return file_list
 
-    def insert_userinfo(self, eng_name, kor_name, age):
-        sql = 'INSERT INTO '
+    def insert_userinfo(self, name_eng, name_kor, age):
+
+        if TEST_MODE:
+            sql = 'INSERT INTO hfe_user_test(age, name_kor, name_eng) VALUES (%s, %s, %s)'
+            get_user_idx_sql = 'SELECT user_idx FROM hfe_user_test WHERE name_eng = %s'
+        else:
+            sql = 'INSERT INTO hfe_user(age, name_kor, name_eng) VALUES (%s, %s, %s)'
+            get_user_idx_sql = 'SELECT user_idx FROM hfe_user WHERE name_eng = %s'
+
+
+        user_idx = 0
+        try:
+            conn = self.conn
+            cursor = conn.cursor()
+            cursor.execute(sql, (age, name_kor, name_eng))
+            cursor.execute(get_user_idx_sql, (name_eng,))
+            user_idx = cursor.fetchone()['user_idx']
+            conn.commit()
+        except pymysql.err.MySQLError as mse:
+            print(mse)
+            sys.exit(1)
+        except Exception as e:
+            print(e)
+        finally:
+            cursor.close()
+
+        return user_idx
+
+    def insert_ex_data(self):
+        pass
 
 
     def parsing_name(self, filename):
@@ -94,16 +143,16 @@ class DataCollector:
         hand_side_merged = splited_filename[1]
         # 왼손/오른손 여부
         hand_side = hand_side_merged[:-1]
-        if hand_side == 'left':
+        if hand_side.lower() == 'left':
             hand_side = '0'
-        elif hand_side == 'right':
+        elif hand_side.lower() == 'right':
             hand_side = '1'
         # 실험 회차
-        num_of_ex = hand_side_merged[-1]
+        ex_sequence = hand_side_merged[-1]
         # 상자 크기
         box_size = splited_filename[2]
 
-        return num_of_ex, hand_side, box_size
+        return ex_sequence, hand_side, box_size
 
     def run_collector(self):
 
@@ -111,11 +160,11 @@ class DataCollector:
 
         for file_name in file_list:
             # 회차, 왼손/오른손, 박스사이즈 리턴
-            num_of_ex, hand_side, box_size = self.parsing_name(filename=file_name)
+            ex_sequence, hand_side, box_size = self.parsing_name(filename=file_name)
 
-            print(num_of_ex, hand_side, box_size)
+            print(ex_sequence, hand_side, box_size)
 
-            self.read_excel(file_name, num_of_ex=num_of_ex, hand_side=hand_side, box_size=box_size)
+            self.read_excel(file_name, ex_sequence=ex_sequence, hand_side=hand_side, box_size=box_size)
 
 
 if __name__ == '__main__':
@@ -130,6 +179,6 @@ if __name__ == '__main__':
 
     print("폴더 내의 엑셀 파일을 스캔합니다.\n")
 
-    collector = DataCollector(name_eng=name_eng)
+    collector = DataCollector(name_eng=name_eng, name_kor=name_kor, age=age)
 
     collector.run_collector()
